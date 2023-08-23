02789251dc1a0e326278481a853582735f9685aa
Anatolii Korol <41110370+akorol98@users.noreply.github.com>
2023-08-23 11:23:34 +0200

    smaller SF added

--- models/shower_flow.py
+++ models/shower_flow.py
@@ -116,3 +116,66 @@ def compile_HybridTanH_model(num_blocks, num_inputs, num_cond_inputs, device):
     flow_dist = dist.ConditionalTransformedDistribution(base_dist, transforms)
 
     return modules, flow_dist
+
+
+
+def compile_HybridTanH_model_s(num_blocks, num_inputs, num_cond_inputs, device, input_dim_multiplier=10):
+    # the latent space distribution: choosing a 2-dim Gaussian
+    base_dist = dist.Normal(torch.zeros(num_inputs).to(device), torch.ones(num_inputs).to(device))
+
+    input_dim = num_inputs
+    count_bins = 8
+    transforms = []
+    transforms2 = []
+      
+    input_dim = num_inputs
+    split_dim = num_inputs//2
+    param_dims1 = [input_dim-split_dim, input_dim-split_dim]
+    param_dims2 = [input_dim * count_bins, input_dim * count_bins, input_dim * (count_bins - 1), input_dim * count_bins]
+
+    torch.manual_seed(42)   # important to reproduce the permutations 
+
+    for i in range(num_blocks):
+        
+                    
+        hypernet = ConditionalDenseNN(split_dim, num_cond_inputs, [input_dim*input_dim_multiplier, input_dim*input_dim_multiplier], param_dims1)
+        #hypernet.apply(init_weights)
+        ctf = ConditionalAffineCouplingTanH(split_dim, hypernet)
+        transforms2.append(ctf)
+        transforms.append(ctf)
+        
+        perm = torch.randperm(input_dim, dtype=torch.long).to(device)
+        ff = T.Permute(perm)
+        transforms.append(ff)
+
+
+        
+        hypernet = ConditionalDenseNN(split_dim, num_cond_inputs, [input_dim*input_dim_multiplier, input_dim*input_dim_multiplier], param_dims1)
+        #hypernet.apply(init_weights)
+        ctf = ConditionalAffineCouplingTanH(split_dim, hypernet)
+        transforms2.append(ctf)
+        transforms.append(ctf)
+        
+        perm = torch.randperm(input_dim, dtype=torch.long).to(device)
+        ff = T.Permute(perm)
+        transforms.append(ff)
+        
+        
+        
+        perm = torch.randperm(input_dim, dtype=torch.long).to(device)
+        ff = T.Permute(perm)
+        transforms.append(ff)
+        
+        hypernet = DenseNN(num_cond_inputs, [input_dim*input_dim_multiplier, input_dim*input_dim_multiplier], param_dims2)
+        #hypernet.apply(init_weights)
+        ctf = T.ConditionalSpline(hypernet, input_dim, count_bins)
+        transforms2.append(ctf)
+        transforms.append(ctf)
+             
+        
+        
+    modules = nn.ModuleList(transforms2)
+
+    flow_dist = dist.ConditionalTransformedDistribution(base_dist, transforms)
+
+    return modules, flow_dist
