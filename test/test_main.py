113aa8c0c34b436943054054896cc2927e605144
Henry Day-Hall <henry.day-hall@desy.de>
2024-01-24 20:44:05 +0100

    changed log dir to a temporary dir

--- test/test_main.py
+++ test/test_main.py
@@ -5,21 +5,19 @@ path_root1 = Path(__file__).parents[1]
 sys.path.append(str(path_root1))
 
 import os
+import pytest
 from configs import Configs
 from main import main
 
 
-def test_main():
+def test_main(tmpdir):
     # set a test config
     cfg = Configs()
     # no logging for tests, as we would need a comet key
     cfg.log_comet = False
     cfg.dataset_path = os.path.join(path_root1, 'test', 'mini_data_sample.hdf5')
     cfg.max_iters = 2
-    #cfg.max_points = 2
-    #cfg.workers = 1
-    #cfg.val_freq = 1
-    #cfg.test_size = 1
+    cfg.logdir = tmpdir.mkdir("logs")
     main(cfg)
 
 
