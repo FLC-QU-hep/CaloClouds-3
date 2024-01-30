c523ee68ceb88818e350e2d965363e528cacecff
Henry Day-Hall <henry.day-hall@cern.ch>
2024-01-30 15:08:39 +0100

    Stronger testing

--- test/test_cd.py
+++ test/test_cd.py
@@ -5,10 +5,9 @@ path_root1 = Path(__file__).parents[1]
 sys.path.append(str(path_root1))
 
 import os
-import glob
 import pytest
 from configs import Configs
-from main import main
+from cd import main
 
 
 def test_main(tmpdir):
@@ -18,10 +17,9 @@ def test_main(tmpdir):
     cfg.log_comet = False
     cfg.dataset_path = os.path.join(path_root1, 'test', 'mini_data_sample.hdf5')
     cfg.max_iters = 2
-    cfg.logdir = tmpdir.mkdir("logs")
+    cfg.logdir = os.path.join(path_root1, 'test')
+    cfg.model_path = "mini_ckpt_sample.pt"
     cfg.device = 'cpu'
     main(cfg)
-    # check the model ckpt was created
-    assert glob.glob(f"{cfg.logdir}/{cfg.name}*/ckpt_*.pt")
 
 
