886bb72957ca69edef4defdbc0439614a5e3a84f
Henry Day-Hall <henry.day-hall@cern.ch>
2024-02-19 18:35:03 +0100

    started writing tests for the detector map

--- test/test_detector_map.py
+++ test/test_detector_map.py
@@ -2,6 +2,7 @@
 # get the folder above on the path
 import sys
 from pathlib import Path
+
 path_root1 = Path(__file__).parents[1]
 sys.path.append(str(path_root1))
 
@@ -38,13 +39,14 @@ def test_create_map():
 
     half_cell_size = 0.5
     cell_thickness = 0.1
-    layer_bottom_pos = np.linspace(0, 4, n_layers) - cell_thickness/2
+    layer_bottom_pos = np.linspace(0, 4, n_layers) - cell_thickness / 2
 
-
-    layers, offset = detector_map.create_map(X, Y, Z, layer_bottom_pos, half_cell_size, cell_thickness)
-    npt.assert_allclose(offset, 1.)
+    layers, offset = detector_map.create_map(
+        X, Y, Z, layer_bottom_pos, half_cell_size, cell_thickness
+    )
+    npt.assert_allclose(offset, 1.0)
     # all layers should be identical
-    for i in range(n_layers-1):
+    for i in range(n_layers - 1):
         npt.assert_allclose(layers[i]["xedges"], layers[-1]["xedges"])
         npt.assert_allclose(layers[i]["zedges"], layers[-1]["zedges"])
         npt.assert_allclose(layers[i]["grid"], layers[-1]["grid"])
@@ -55,10 +57,6 @@ def test_create_map():
     z_edges_0 = layers[0]["zedges"]
     assert np.all(z_points >= z_edges_0[:-1])
     assert np.all(z_points <= z_edges_0[1:])
-
-
-    
-
 
 
 def assert_layer_like(layer):
@@ -76,13 +74,14 @@ def assert_layer_like(layer):
 def test_normalise_map():
     x_edges_before = np.linspace(0, 5, 5)
     z_edges_before = np.linspace(-5, 5, 5)
-    MAP = [{'xedges': x_edges_before, 'zedges': z_edges_before, 'grid': np.ones((4, 4))}]
+    MAP = [
+        {"xedges": x_edges_before, "zedges": z_edges_before, "grid": np.ones((4, 4))}
+    ]
     MAP = detector_map.normalise_map(MAP)
-    x_edges_after = MAP[0]['xedges']
-    z_edges_after = MAP[0]['zedges']
+    x_edges_after = MAP[0]["xedges"]
+    z_edges_after = MAP[0]["zedges"]
     npt.assert_allclose(x_edges_after, np.linspace(-1, 1, 5))
     npt.assert_allclose(z_edges_after, np.linspace(-1, 1, 5))
-
 
 
 def test_split_to_layers():
@@ -97,7 +96,9 @@ def test_split_to_layers():
 def test_layer_to_cells():
     x_edges_before = np.linspace(0, 5, 5)
     z_edges_before = np.linspace(-5, 5, 5)
-    MAP = [{'xedges': x_edges_before, 'zedges': z_edges_before, 'grid': np.ones((4, 4))}]
+    MAP = [
+        {"xedges": x_edges_before, "zedges": z_edges_before, "grid": np.ones((4, 4))}
+    ]
 
     # shouldn't choke on empty input
     cells = detector_map.layer_to_cells(MAP, [], [], [])
@@ -106,24 +107,58 @@ def test_layer_to_cells():
 
     # simple sample
     x_points = [0.5, 0.5, 4.5, 5.5]
-    z_points = [0.5, 0.5, 4., 4.5]
+    z_points = [0.5, 0.5, 4.0, 4.5]
     e_points = [1, 1, 3, 1]
 
     cells = detector_map.layer_to_cells(MAP, x_points, z_points, e_points)
 
     expected_cells = np.zeros((5, 5))
-    expected_cells[2, 0] = 2.
-    expected_cells[4, 4] = 3.
+    expected_cells[2, 0] = 2.0
+    expected_cells[4, 4] = 3.0
     npt.assert_allclose(cells, expected_cells)
-    
 
 
 def test_points_to_cells():
-    pass
+    x_edges_before = np.linspace(0, 5, 5)
+    z_edges_before = np.linspace(-5, 5, 5)
+    MAP = [
+        {"xedges": x_edges_before, "zedges": z_edges_before, "grid": np.ones((4, 4))},
+        {"xedges": x_edges_before, "zedges": z_edges_before, "grid": np.ones((4, 4))}
+    ]
+    layer_bottom_pos = np.array([0, 1])
+
+    # shouldn't choke on empty input
+    cells = detector_map.points_to_cells([], MAP, layer_bottom_pos)
+    expected_cells = np.zeros((2, 5, 5))
+    npt.assert_allclose(cells, expected_cells)
+
+    # simple sample
+    x_points = [0.5, 0.5, 4.5, 5.5, 0.5]
+    y_points = [0.5, 0.5, 0.5, 0.5, 1.5]
+    z_points = [0.5, 0.5, 4.0, 4.5, 0.5]
+    e_points = [1, 1, 3, 1, 5]
+
+    cells = detector_map.layer_to_cells(MAP, x_points, z_points, e_points)
+
+    expected_cells = np.zeros((2, 5, 5))
+    expected_cells[0, 2, 0] = 2.0
+    expected_cells[0, 4, 4] = 3.0
+    expected_cells[1, 2, 0] = 5.0
+    npt.assert_allclose(cells, expected_cells)
 
 
 def test_cells_to_points():
-    pass
+    MAP = [
+        {"xedges": x_edges_before, "zedges": z_edges_before, "grid": np.ones((4, 4))},
+        {"xedges": x_edges_before, "zedges": z_edges_before, "grid": np.ones((4, 4))}
+    ]
+    layer_bottom_pos = np.array([0, 1])
+    cells = np.zeros((2, 5, 5))
+    cells[0, 2, 0] = 2.0
+    cells[0, 4, 4] = 3.0
+    cells[1, 2, 0] = 5.0
+    pass  # TODO
+
 
 def test_get_projections():
     pass
