c523932d38d98743b20fe1a3fc90d5a98d9bd4a7
Henry Day-Hall <henry.day-hall@cern.ch>
2024-03-18 18:34:07 +0100

    Added a more helpful error message on the mirroring test

--- test/test_notebook_mirror.py
+++ test/test_notebook_mirror.py
@@ -11,6 +11,12 @@ sys.path.append(str(path_root1))
 from programming_utils import mirror_notebook
 import os
 
+_generic_hooks_notice = (
+    "If this is failing, you might not have the pre-commit hooks installed. "
+    + "Review the hooks in .githooks to check you are happy with them, "
+    + "then install them with `make enable-git-hooks`"
+)
+
 
 def assert_has_mirror(notebook_path):
     """
@@ -20,14 +26,14 @@ def assert_has_mirror(notebook_path):
     mirror_file = mirror_notebook.get_mirror_path(notebook_path)
     assert os.path.isfile(
         mirror_file
-    ), f"Notebook {notebook_path} does not have a mirror file {mirror_file}"
+    ), f"Notebook {notebook_path} does not have a mirror file {mirror_file}. {_generic_hooks_notice}"
     # check the content
     expected_mirror = mirror_notebook.extract(notebook_path)
     with open(mirror_file, "r") as f:
         actual_mirror = f.read()
     assert (
         expected_mirror.strip() == actual_mirror.strip()
-    ), f"Mirror file {mirror_file} does not match the notebook {notebook_path}"
+    ), f"Mirror file {mirror_file} does not match the notebook {notebook_path}. {_generic_hooks_notice}"
 
 
 def test_check_notebooks():
